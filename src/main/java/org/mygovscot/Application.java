27a1e317b79fe186d95bd42e74c8d76e35052223
Tim Squires <Tim.Squires@scotland.gsi.gov.uk>
2014-10-16 15:46:44 +0100

    MGVI-1706 TASK Added mygov configuration using yml

--- src/main/java/org/mygovscot/Application.java
+++ src/main/java/org/mygovscot/Application.java
@@ -1,7 +1,10 @@
 package org.mygovscot;
 
+import org.mygovscot.beta.config.BetaConfigInitializer;
+import org.mygovscot.beta.config.Dump;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,14 +13,15 @@ import org.springframework.context.annotation.Configuration;
 @EnableAutoConfiguration
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+    public String getConfiguration() {
+        return "business-rates-rest-service";
     }
 
-    public void notAUtility() {
-        // This is to stop Sonar thinking that this is a utility class.
-        // spring-boot requires a default constructor. Checkstyle says that a
-        // class with only static methods is a utility class and should not have
-        // a default constructor.
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplicationBuilder(Application.class).showBanner(false).application();
+        application.addInitializers(new BetaConfigInitializer());
+        application.run(args);
+        Dump.main(args);
     }
+
 }
