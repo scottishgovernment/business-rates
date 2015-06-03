8d1c64b521f589906b68862892999d6d4b04b9dd
Rupert Smith <rupert.smith2@scotland.gsi.gov.uk>
2015-06-03 15:07:59 +0100

    Corrected the location of the logs.

--- business-rates-deb/src/deb/run.sh
+++ business-rates-deb/src/deb/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 . /etc/profile
-/usr/bin/java -jar /opt/business-rates/*.jar >> /var/log/business-rates.log 2>&1
+/usr/bin/java -jar /opt/business-rates/*.jar >> /var/log/business-rates/business-rates.log 2>&1
