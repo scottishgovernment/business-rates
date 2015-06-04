355540419a6284091dbaa962f8e545cf2c262146
Rupert Smith <rupert.smith2@scotland.gsi.gov.uk>
2015-06-04 15:03:22 +0100

    Added top-level error handling to main thread, and to all servlet threads. All unhandled exceptions will now be logged as alerts.

--- service/src/main/java/org/mygovscot/Application.java
+++ service/src/main/java/org/mygovscot/Application.java
@@ -2,6 +2,9 @@ package org.mygovscot;
 
 import org.mygovscot.beta.config.BetaConfigInitializer;
 import org.mygovscot.beta.config.Dump;
+import org.mygovscot.util.error.toplevelhandlers.SLF4JStrictTopLevelErrorHandler;
+import org.mygovscot.util.error.toplevelhandlers.TopLevelErrorHandler;
+import org.mygovscot.util.servlet.filter.ErrorHandlerFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -12,22 +15,29 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 
+import javax.servlet.Filter;
+
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
 public class Application {
+    /** For logging all unhandled exceptions. */
+    public static final TopLevelErrorHandler TOP_LEVEL_ERROR_HANDLER = new SLF4JStrictTopLevelErrorHandler();
 
     public String getConfiguration() {
         return "business-rates-rest-service";
     }
 
     public static void main(String[] args) {
-        SpringApplication application = new SpringApplicationBuilder(Application.class).showBanner(false).application();
-        application.addInitializers(new BetaConfigInitializer());
-        application.run(args);
-        Dump.main(args);
+        try {
+            SpringApplication application = new SpringApplicationBuilder(Application.class).showBanner(false).application();
+            application.addInitializers(new BetaConfigInitializer());
+            application.run(args);
+            Dump.main(args);
+        } catch (Throwable t) {
+            TOP_LEVEL_ERROR_HANDLER.handleThrowable(t);
+        }
     }
-
 
     @Bean
     public EmbeddedServletContainerCustomizer containerCustomizer() {
@@ -39,5 +49,10 @@ public class Application {
 
             container.addErrorPages(error401Page, error404Page, error500Page);
         };
+    }
+
+    @Bean
+    public Filter errorHandlerFilter() {
+        return new ErrorHandlerFilter();
     }
 }
