843c3dc6f187ede11965ea2b36c88b4022c0ea9f
Martin Ellis <martin.ellis@gov.scot>
2016-06-14 17:15:33 +0100

    MGS-1341 Remove dependency on mygovscot_utils.

--- service/src/main/java/org/mygovscot/Application.java
+++ service/src/main/java/org/mygovscot/Application.java
@@ -2,9 +2,6 @@ package org.mygovscot;
 
 import org.mygovscot.beta.config.BetaConfigInitializer;
 import org.mygovscot.beta.config.Dump;
-import org.mygovscot.util.error.toplevelhandlers.SLF4JStrictTopLevelErrorHandler;
-import org.mygovscot.util.error.toplevelhandlers.TopLevelErrorHandler;
-import org.mygovscot.util.servlet.filter.ErrorHandlerFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -15,28 +12,20 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 
-import javax.servlet.Filter;
-
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
 public class Application {
-    /** For logging all unhandled exceptions. */
-    public static final TopLevelErrorHandler TOP_LEVEL_ERROR_HANDLER = new SLF4JStrictTopLevelErrorHandler();
 
     public String getConfiguration() {
         return "business-rates-rest-service";
     }
 
     public static void main(String[] args) {
-        try {
-            SpringApplication application = new SpringApplicationBuilder(Application.class).showBanner(false).application();
-            application.addInitializers(new BetaConfigInitializer());
-            application.run(args);
-            Dump.main(args);
-        } catch (Throwable t) {
-            TOP_LEVEL_ERROR_HANDLER.handleThrowable(t);
-        }
+        SpringApplication application = new SpringApplicationBuilder(Application.class).showBanner(false).application();
+        application.addInitializers(new BetaConfigInitializer());
+        application.run(args);
+        Dump.main(args);
     }
 
     @Bean
@@ -51,8 +40,4 @@ public class Application {
         };
     }
 
-    @Bean
-    public Filter errorHandlerFilter() {
-        return new ErrorHandlerFilter();
-    }
 }
